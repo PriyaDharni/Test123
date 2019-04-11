4a7d021a42673be98757b6a429b6ec67312630d1
639164 <639164@PC320577.cts.com>
2019-04-11 18:20:08 +0530

    Update

---
+++ Test_1234/src/Page_123/Page2_1234.java
@@ -0,0 +1,9 @@
+package Page_123;
+
+public class Page2_1234 {
+public static void main(String[] args) {
+	System.out.println("HELLO WORLD");
+}
+
+
+}
---
+++ Test_1234/src/Page_123/Page3_1234.java
@@ -0,0 +1,10 @@
+package Page_123;
+
+public class Page3_1234 {
+public static void main(String [] args)
+{
+	Integer x=5;
+	Integer y=5;
+	System.out.println(x+y);
+}
+}
